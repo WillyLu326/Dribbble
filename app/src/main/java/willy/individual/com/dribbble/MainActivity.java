9f4f867aa44a952015c40fe9ffcc9ac46ab40893
WillyLu326 <luzh0326@gmail.com>
2016-12-27 08:46:10 -0500

    Dribbble

--- app/src/main/java/willy/individual/com/dribbble/MainActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
+    // Dribbble Native App
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
