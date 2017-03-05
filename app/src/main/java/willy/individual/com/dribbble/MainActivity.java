37fff295d94f228f3ca68d0743bbfc583a730a9f
WillyLu326 <luzh0326@gmail.com>
2017-03-05 01:37:51 -0500

    Add ButterKnife gradle

--- app/src/main/java/willy/individual/com/dribbble/MainActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/MainActivity.java
@@ -2,12 +2,21 @@ package willy.individual.com.dribbble;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text) TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        tv.setText("Willy Lu");
     }
 }
