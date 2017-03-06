fc55e960e61f9ce4237178b8252575637c70205c
WillyLu326 <luzh0326@gmail.com>
2017-03-06 00:35:04 -0500

    Add Login xml

---
+++ app/src/main/java/willy/individual/com/dribbble/views/LoginActivity.java
@@ -0,0 +1,12 @@
+package willy.individual.com.dribbble.views;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhenglu on 3/6/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+
+}
