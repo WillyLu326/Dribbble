8f2845c758424ad88bb407ba945773d3bbdc4f0e
WillyLu326 <luzh0326@gmail.com>
2017-03-06 00:37:55 -0500

    Add Login Activity

--- app/src/main/java/willy/individual/com/dribbble/views/LoginActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/views/LoginActivity.java
@@ -1,6 +1,10 @@
 package willy.individual.com.dribbble.views;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import willy.individual.com.dribbble.R;
 
 /**
  * Created by zhenglu on 3/6/17.
@@ -8,5 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
