4356fd74a2d45c0d3e62498aa27ce8ee163f3a7d
WillyLu326 <luzh0326@gmail.com>
2017-03-06 23:14:19 -0500

    set title after clicking navigation item

--- app/src/main/java/willy/individual/com/dribbble/MainActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/MainActivity.java
@@ -29,12 +29,15 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.drawer_menu_home :
                         Toast.makeText(getApplicationContext(), "Home Clicked", Toast.LENGTH_SHORT).show();
+                        setTitle(R.string.home_title);
                         break;
                     case R.id.drawer_menu_like :
                         Toast.makeText(getApplicationContext(), "Like Clicked", Toast.LENGTH_SHORT).show();
+                        setTitle(R.string.favoriate_title);
                         break;
                     case R.id.drawer_menu_bucket :
                         Toast.makeText(getApplicationContext(), "Bucket Clicked", Toast.LENGTH_SHORT).show();
+                        setTitle(R.string.bucket_title);
                         break;
                 }
                 drawerLayout.closeDrawers();
