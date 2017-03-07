d116eaee945d895ecac27d8ea9a2d4cfa4742569
WillyLu326 <luzh0326@gmail.com>
2017-03-06 22:52:18 -0500

    active navigation item when it is selected

--- app/src/main/java/willy/individual/com/dribbble/MainActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
 
-                return false;
+                return true;
             }
         });
 
