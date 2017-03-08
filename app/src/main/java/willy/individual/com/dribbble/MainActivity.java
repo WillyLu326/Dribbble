9748c8b22804afd5ceff10c7399c7f658309e11c
WillyLu326 <luzh0326@gmail.com>
2017-03-07 21:46:32 -0500

    using toolbar instead of action bar

--- app/src/main/java/willy/individual/com/dribbble/MainActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -21,12 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.navigation_drawer) NavigationView navigationView;
     @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @BindView(R.id.my_toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
