e90cb84c197226d668f3930616c909c5d95ef751
WillyLu326 <luzh0326@gmail.com>
2017-03-08 22:02:52 -0500

    extract toolbar layout

--- app/src/main/java/willy/individual/com/dribbble/views/LoginActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/views/LoginActivity.java
@@ -1,20 +1,35 @@
 package willy.individual.com.dribbble.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import willy.individual.com.dribbble.R;
 
-/**
- * Created by zhenglu on 3/6/17.
- */
-
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.login_btn) TextView loginTv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        setupLoginUI();
+    }
+
+    private void setupLoginUI() {
+        loginTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+            }
+        });
     }
 }
---
+++ app/src/main/java/willy/individual/com/dribbble/views/auth/AuthActivity.java
@@ -0,0 +1,17 @@
+package willy.individual.com.dribbble.views.auth;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhenglu on 3/8/17.
+ */
+
+public class AuthActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
