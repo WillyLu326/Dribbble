16f6f267b80a04390206b74c976ec2678a2dd4c3
WillyLu326 <luzh0326@gmail.com>
2017-03-08 22:12:14 -0500

    add webview

--- app/src/main/java/willy/individual/com/dribbble/views/auth/AuthActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/views/auth/AuthActivity.java
@@ -3,15 +3,29 @@ package willy.individual.com.dribbble.views.auth;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
 
-/**
- * Created by zhenglu on 3/8/17.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import willy.individual.com.dribbble.R;
+
 
 public class AuthActivity extends AppCompatActivity {
+
+    @BindView(R.id.my_toolbar) Toolbar toolbar;
+    @BindView(R.id.web_view) WebView webView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+        ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
+
+        String url = getIntent().getStringExtra("");
+
+        webView.loadUrl(url);
     }
 }
