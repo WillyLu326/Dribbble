200fe4a9a988cb89a6c3d326fa969af23f447905
WillyLu326 <luzh0326@gmail.com>
2017-03-09 00:08:59 -0500

    fix toolbar issues

---
+++ app/src/main/java/willy/individual/com/dribbble/views/auth/Auth.java
@@ -0,0 +1,11 @@
+package willy.individual.com.dribbble.views.auth;
+
+/**
+ * Created by zhenglu on 3/9/17.
+ */
+
+public class Auth {
+
+
+
+}
--- app/src/main/java/willy/individual/com/dribbble/views/auth/AuthActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/views/auth/AuthActivity.java
@@ -1,10 +1,13 @@
 package willy.individual.com.dribbble.views.auth;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.webkit.WebResourceRequest;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
@@ -27,16 +30,48 @@ public class AuthActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         progressBar.setMax(100);
 
         String url = "https://dribbble.com/oauth/authorize?client_id=76048d257d97a98958efb5bdf0ccbc521b793af5725dbab3b67c55a672080bf4&redirect_uri=http://www.zhenglu326.com&scope=public+write&state=willylu";
 
 
+        webView.requestFocus(View.FOCUS_DOWN);
         webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+                progressBar.setProgress(0);
+            }
 
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+            }
         });
 
         webView.loadUrl(url);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home :
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
