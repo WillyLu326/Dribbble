a5fa6c7b1830c32b7ecd27d1d1ff2e7520bf1f5a
WillyLu326 <luzh0326@gmail.com>
2017-03-09 23:56:43 -0500

    create model utils class

--- app/src/main/java/willy/individual/com/dribbble/utils/ModelUtils.java
+++ app/src/main/java/willy/individual/com/dribbble/utils/ModelUtils.java
@@ -1,9 +1,29 @@
 package willy.individual.com.dribbble.utils;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 /**
  * Created by zhenglu on 3/9/17.
  */
 
 public class ModelUtils {
 
+    private static Gson gson = new Gson();
+
+    private static final String SP_KEY = "model_key";
+
+    public static void save(Context context, String key, Object model) {
+        SharedPreferences sp = context.getSharedPreferences(SP_KEY, context.MODE_PRIVATE);
+        sp.edit().putString(key, gson.toJson(model)).apply();
+    }
+
+    public static <T> T read(Context context, String key, TypeToken<T> typeToken) {
+        SharedPreferences sp = context.getSharedPreferences(SP_KEY, context.MODE_PRIVATE);
+        String value = sp.getString(key, "");
+        return gson.fromJson(value, typeToken.getType());
+    }
 }
