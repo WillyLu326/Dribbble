28410d12638839caa430abbd68857d79dcd7875a
WillyLu326 <luzh0326@gmail.com>
2017-03-11 16:40:26 -0500

    setup toolbar of shotActivity

--- app/src/main/java/willy/individual/com/dribbble/views/shot_detail/ShotActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/views/shot_detail/ShotActivity.java
@@ -3,15 +3,36 @@ package willy.individual.com.dribbble.views.shot_detail;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import willy.individual.com.dribbble.R;
 
 
 public class ShotActivity extends AppCompatActivity {
 
+    @BindView(R.id.my_toolbar) Toolbar myToolBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shot_detail);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(myToolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("Shot");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home :
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
