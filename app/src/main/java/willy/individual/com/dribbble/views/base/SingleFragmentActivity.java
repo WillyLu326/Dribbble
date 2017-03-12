fadbd0bdd5ea34e765a478338e1d2309ff1f89f3
WillyLu326 <luzh0326@gmail.com>
2017-03-12 01:13:15 -0500

    add singleFragmentActivity

---
+++ app/src/main/java/willy/individual/com/dribbble/views/base/SingleFragmentActivity.java
@@ -0,0 +1,11 @@
+package willy.individual.com.dribbble.views.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhenglu on 3/12/17.
+ */
+
+public class SingleFragmentActivity extends AppCompatActivity {
+
+}
