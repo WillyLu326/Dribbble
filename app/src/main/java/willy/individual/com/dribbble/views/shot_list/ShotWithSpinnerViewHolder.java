076ee22e2d88ab6c548e5fe29c4519c40f44030a
WillyLu326 <luzh0326@gmail.com>
2017-03-12 20:45:08 -0400

    finish shot with spinner viewholder

--- app/src/main/java/willy/individual/com/dribbble/views/shot_list/ShotWithSpinnerViewHolder.java
+++ app/src/main/java/willy/individual/com/dribbble/views/shot_list/ShotWithSpinnerViewHolder.java
@@ -2,14 +2,23 @@ package willy.individual.com.dribbble.views.shot_list;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import willy.individual.com.dribbble.R;
 
 /**
  * Created by zhenglu on 3/12/17.
  */
 
 public class ShotWithSpinnerViewHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.shot_item_with_spinner_image) ImageView shotWithSpinnerImage;
+    @BindView(R.id.shot_item_with_spinner_view_count) TextView shotWithSpinnerViewsCountTv;
+    @BindView(R.id.shot_item_with_spinner_like) TextView shotWithSpinnerLikesCountTv;
+    @BindView(R.id.shot_item_with_spinner_bucket) TextView shotWithSpinnerBucketsCountTv;
 
     public ShotWithSpinnerViewHolder(View itemView) {
         super(itemView);
