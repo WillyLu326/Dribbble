5a82b5ccc6607e81e55de67513b331e4bd1a2927
WillyLu326 <luzh0326@gmail.com>
2017-03-14 17:57:24 -0400

    add shot properties and ShotUser

--- app/src/main/java/willy/individual/com/dribbble/models/Shot.java
+++ app/src/main/java/willy/individual/com/dribbble/models/Shot.java
@@ -1,5 +1,6 @@
 package willy.individual.com.dribbble.models;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -8,13 +9,33 @@ import java.util.Map;
 
 public class Shot {
 
+    public int id;
+    public String title;
+    public String description;
+    public int width;
+    public int height;
+    public Map<String, String> images;
     public int views_count;
     public int likes_count;
+    public int comments_count;
+    public int attachments_count;
+    public int rebounds_count;
     public int butckets_count;
-
-    public Map<String, String> images;
+    public String created_at;
+    public String updated_at;
+    public String html_url;
+    public String attachments_url;
+    public String buckets_url;
+    public String comments_url;
+    public String likes_url;
+    public String projects_url;
+    public String rebounds_url;
+    public boolean animated;
+    public List<String> tags;
+    public ShotUser user;
 
     public String getImageUrl() {
         return images.containsKey("hidpi") ? images.get("hidpi") : images.get("normal");
     }
 }
+
---
+++ app/src/main/java/willy/individual/com/dribbble/models/ShotUser.java
@@ -0,0 +1,40 @@
+package willy.individual.com.dribbble.models;
+
+import java.util.Map;
+
+/**
+ * Created by zhenglu on 3/14/17.
+ */
+
+public class ShotUser {
+
+    public int id;
+    public String name;
+    public String username;
+    public String html_url;
+    public String avatar_url;
+    public String bio;
+    public String location;
+    public Map<String, String> links;
+    public int buckets_count;
+    public int comments_received_count;
+    public int followers_count;
+    public int followings_count;
+    public int likes_count;
+    public int likes_received_count;
+    public int projects_count;
+    public int rebounds_received_count;
+    public int shots_count;
+    public int teams_count;
+    public boolean can_upload_shot;
+    public String type;
+    public boolean pro;
+    public String buckets_url;
+    public String followers_url;
+    public String following_url;
+    public String likes_url;
+    public String shots_url;
+    public String teams_url;
+    public String created_at;
+    public String updated_at;
+}
