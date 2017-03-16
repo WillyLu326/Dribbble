9478df265997c2c6d2579ce4a161975df78e174e
WillyLu326 <luzh0326@gmail.com>
2017-03-16 19:11:12 -0400

    add todo

---
+++ app/src/main/java/willy/individual/com/dribbble/TODO.java
@@ -0,0 +1,19 @@
+package willy.individual.com.dribbble;
+
+/**
+ * Created by zhenglu on 3/16/17.
+ */
+
+public class TODO {
+
+    /**
+     *  TODO
+     *  1. Share Button Function
+     *  2. Bucket Function
+     *  3. Comment Function
+     *  4. User Following
+     *  5. User Follower
+     *  6. Check User Profile
+     *  7. Pull to Refresh
+     */
+}
