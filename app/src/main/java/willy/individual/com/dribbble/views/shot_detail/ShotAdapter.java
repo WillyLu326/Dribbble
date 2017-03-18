f0cbf1fbf48e8e707892991e4f8be201592be8b6
WillyLu326 <luzh0326@gmail.com>
2017-03-18 19:53:09 -0400

    setup comment ui

--- app/src/main/java/willy/individual/com/dribbble/views/shot_detail/ShotAdapter.java
+++ app/src/main/java/willy/individual/com/dribbble/views/shot_detail/ShotAdapter.java
@@ -150,7 +150,7 @@ public class ShotAdapter extends RecyclerView.Adapter {
             ShotCommentViewHolder shotCommentViewHolder = (ShotCommentViewHolder) holder;
             shotCommentViewHolder.commentNameTv.setText(comment.user.name);
             shotCommentViewHolder.commentContentTv.setText(Html.fromHtml(comment.body, 1));
-            shotCommentViewHolder.commentDateTv.setText(comment.updated_at.toString());
+            shotCommentViewHolder.commentDateTv.setText(comment.updated_at.toString() + "  |  ");
             DraweeController controller = Fresco.newDraweeControllerBuilder()
                     .setUri(comment.user.avatar_url)
                     .setAutoPlayAnimations(true)
