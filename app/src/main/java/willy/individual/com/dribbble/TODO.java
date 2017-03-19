651e2184fd61176c7403f72441dd635d44272b5a
WillyLu326 <luzh0326@gmail.com>
2017-03-18 21:38:07 -0400

    change placeholder color

--- app/src/main/java/willy/individual/com/dribbble/TODO.java
+++ app/src/main/java/willy/individual/com/dribbble/TODO.java
@@ -10,10 +10,10 @@ public class TODO {
      *  TODO
      *  1. Share Button Function
      *  2. Bucket Function
-     *  3. Comment Function
+     *  3. Comment Function ==> Done
      *  4. User Following
      *  5. User Follower
      *  6. Check User Profile
-     *  7. Pull to Refresh
+     *  7. Pull to Refresh  ==> Done
      */
 }
