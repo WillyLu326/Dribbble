797060140eb6eaf26bcfdb56db974f2d10258184
WillyLu326 <luzh0326@gmail.com>
2017-03-18 23:00:41 -0400

    version 2 ui change

--- app/src/main/java/willy/individual/com/dribbble/TODO.java
+++ app/src/main/java/willy/individual/com/dribbble/TODO.java
@@ -1,21 +1,14 @@
 package willy.individual.com.dribbble;
 
-/**
- * Created by zhenglu on 3/16/17.
- */
 
 public class TODO {
-
-
 
     /**
      *  TODO ==> Version 2.0.0
      *  1. Share Button Function
      *  2. Bucket Function
-     *  3. Comment Function ==> Done
-     *  4. User Following
-     *  5. User Follower
-     *  6. Check User Profile
-     *  7. Pull to Refresh  ==> Done
+     *  3. User Following
+     *  4. User Follower
+     *  5. Check User Profile
      */
 }
