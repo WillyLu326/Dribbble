63d2e098cfa679fd49821cd8b5c453bad5a1096b
WillyLu326 <luzh0326@gmail.com>
2017-03-19 01:12:46 -0400

    add popup menu

--- app/src/main/java/willy/individual/com/dribbble/views/shot_imgae_activity/ShotImageActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/views/shot_imgae_activity/ShotImageActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
@@ -14,12 +18,12 @@ import com.facebook.drawee.view.SimpleDraweeView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import willy.individual.com.dribbble.R;
-import willy.individual.com.dribbble.views.shot_detail.ShotActivity;
 import willy.individual.com.dribbble.views.shot_detail.ShotAdapter;
 
 
 public class ShotImageActivity extends AppCompatActivity {
 
+    @BindView(R.id.shot_image_linear_layout) LinearLayout shotImageLinearLayout;
     @BindView(R.id.activity_shot_image) SimpleDraweeView shotImage;
 
     @Override
@@ -29,6 +33,7 @@ public class ShotImageActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setupImageUi();
+        setupShotImageLinearLayout();
     }
 
     private void setupImageUi() {
@@ -38,18 +43,36 @@ public class ShotImageActivity extends AppCompatActivity {
                 .setAutoPlayAnimations(true)
                 .build();
         shotImage.setController(controller);
+    }
 
-        shotImage.setOnClickListener(new View.OnClickListener() {
+    private void setupShotImageLinearLayout() {
+        shotImageLinearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
 
-        shotImage.setOnLongClickListener(new View.OnLongClickListener() {
+        shotImageLinearLayout.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                Toast.makeText(getApplicationContext(), "Save Image", Toast.LENGTH_SHORT).show();
+                PopupMenu popup = new PopupMenu(ShotImageActivity.this, shotImage);
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+                popup.setGravity(Gravity.CENTER);
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.popup_save :
+                                Toast.makeText(getApplicationContext(), "Save Image", Toast.LENGTH_SHORT).show();
+                            case R.id.popup_cancel :
+                                finish();
+                        }
+                        return false;
+                    }
+                });
+
+                popup.show();
                 return true;
             }
         });
