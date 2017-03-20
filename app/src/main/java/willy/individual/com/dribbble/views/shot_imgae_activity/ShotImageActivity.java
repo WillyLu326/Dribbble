30994fab6d0079faae829eeb618511b100bd5f50
WillyLu326 <luzh0326@gmail.com>
2017-03-19 20:06:31 -0400

    implement context menu on shotImageActivity

--- app/src/main/java/willy/individual/com/dribbble/views/shot_imgae_activity/ShotImageActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/views/shot_imgae_activity/ShotImageActivity.java
@@ -1,14 +1,14 @@
 package willy.individual.com.dribbble.views.shot_imgae_activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.PopupMenu;
-import android.view.Gravity;
+import android.text.TextUtils;
+import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
@@ -23,8 +23,9 @@ import willy.individual.com.dribbble.views.shot_detail.ShotAdapter;
 
 public class ShotImageActivity extends AppCompatActivity {
 
-    @BindView(R.id.shot_image_linear_layout) LinearLayout shotImageLinearLayout;
+    @BindView(R.id.shot_image_linear_layout) View shotImageLinearLayout;
     @BindView(R.id.activity_shot_image) SimpleDraweeView shotImage;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,7 +34,29 @@ public class ShotImageActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setupImageUi();
+        registerForContextMenu(shotImageLinearLayout);
+
         setupShotImageLinearLayout();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Select an Action");
+        menu.add(0, v.getId(), 0, "Save");
+        menu.add(0, v.getId(), 0, "Cancel");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (TextUtils.equals(item.getTitle(), "Save")) {
+            Toast.makeText(getApplicationContext(), "Save", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.equals(item.getTitle(), "Cancel")) {
+            Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_SHORT).show();
+        } else {
+            return false;
+        }
+        return true;
     }
 
     private void setupImageUi() {
@@ -49,32 +72,9 @@ public class ShotImageActivity extends AppCompatActivity {
         shotImageLinearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
-            }
-        });
-
-        shotImageLinearLayout.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                PopupMenu popup = new PopupMenu(ShotImageActivity.this, shotImage);
-                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
-                popup.setGravity(Gravity.CENTER);
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.popup_save :
-                                Toast.makeText(getApplicationContext(), "Save Image", Toast.LENGTH_SHORT).show();
-                            case R.id.popup_cancel :
-                                finish();
-                        }
-                        return false;
-                    }
-                });
-
-                popup.show();
-                return true;
+              finish();
             }
         });
     }
+
 }
