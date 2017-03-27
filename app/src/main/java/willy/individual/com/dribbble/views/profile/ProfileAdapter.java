be4e911e9a09e02c767f3647f7df6f7f37496730
WillyLu326 <luzh0326@gmail.com>
2017-03-26 23:55:00 -0400

    update profile adapter

---
+++ app/src/main/java/willy/individual/com/dribbble/views/profile/ProfileAdapter.java
@@ -0,0 +1,60 @@
+package willy.individual.com.dribbble.views.profile;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import willy.individual.com.dribbble.R;
+import willy.individual.com.dribbble.models.User;
+
+/**
+ * Created by zhenglu on 3/26/17.
+ */
+
+public class ProfileAdapter extends RecyclerView.Adapter {
+
+    private static final int PROFILE_INFO_TYPE = 0;
+    private User user;
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == PROFILE_INFO_TYPE) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.profile_info, parent, false);
+            return new ProfileInfoViewHolder(view);
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (getItemViewType(position) == PROFILE_INFO_TYPE) {
+            ProfileInfoViewHolder profileInfoViewHolder = (ProfileInfoViewHolder) holder;
+            profileInfoViewHolder.profileUsername.setText(user.name);
+            profileInfoViewHolder.profileLocation.setText(user.location);
+            profileInfoViewHolder.profileDescription.setText(user.bio);
+            DraweeController controller = Fresco.newDraweeControllerBuilder()
+                    .setUri(user.avatar_url)
+                    .setAutoPlayAnimations(true)
+                    .build();
+            profileInfoViewHolder.profileAvatar.setController(controller);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return PROFILE_INFO_TYPE;
+        }
+    }
+}
