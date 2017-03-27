9bedd6b46bc0750c694f9bdf6173b05eb19a17e1
WillyLu326 <luzh0326@gmail.com>
2017-03-26 23:34:26 -0400

    add profileActivity

--- app/src/main/java/willy/individual/com/dribbble/views/auth/AuthActivity.java
+++ app/src/main/java/willy/individual/com/dribbble/views/auth/AuthActivity.java
@@ -91,3 +91,4 @@ public class AuthActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
---
+++ app/src/main/java/willy/individual/com/dribbble/views/profile/ProfileActivity.java
@@ -0,0 +1,22 @@
+package willy.individual.com.dribbble.views.profile;
+
+import android.app.Fragment;
+
+import willy.individual.com.dribbble.views.base.SingleFragmentActivity;
+
+/**
+ * Created by zhenglu on 3/26/17.
+ */
+
+public class ProfileActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment newFragment() {
+        return null;
+    }
+
+    @Override
+    protected String getActivityTitle() {
+        return null;
+    }
+}
