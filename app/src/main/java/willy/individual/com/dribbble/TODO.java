bbd9b81a8ca7da5806a38f5e0a10e5235039ec69
WillyLu326 <luzh0326@gmail.com>
2017-03-30 20:42:43 -0400

    update todo

--- app/src/main/java/willy/individual/com/dribbble/TODO.java
+++ app/src/main/java/willy/individual/com/dribbble/TODO.java
@@ -6,7 +6,7 @@ public class TODO {
     /**
      *  Version 2.0.0
      *  TODO
-     *  1. Share Button Function
+     *  1. Share Button Function    ===> Done!!
      *  2. Bucket Function     ===> Done!!
      *      2.1 Bucket Unchoosen Pull to Refresh ===> Done!!
      *      2.2 Bucket Choosen CRUD Operations ===> Done!!
@@ -16,7 +16,7 @@ public class TODO {
      *  5. Check User Profile   ===> Done!!
      *  6. Save Image to Phone  ===> Future Work!!
      *  7. Navigation Drawer Layout ===> Done!!
-     *  8. Logout Exit Dialog
+     *  8. Logout Exit Dialog       ===> Done!!
      *  9. About Dribbble
      *  10. Exception Handler
      */
