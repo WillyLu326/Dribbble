c3b6294130c2dcc8dabd0a68ffc1c39ba83b61dc
WillyLu326 <luzh0326@gmail.com>
2017-04-18 22:29:55 -0400

    update

--- app/src/main/java/willy/individual/com/dribbbow/views/auth/Auth.java
+++ app/src/main/java/willy/individual/com/dribbbow/views/auth/Auth.java
@@ -127,7 +127,7 @@ public class Auth {
         sp.edit().putString(ACCESS_USER_SP_KEY, ModelUtils.convertToString(user, new TypeToken<User>(){})).apply();
     }
 
-
+    //
     public static User loadAuthUser(Context context) {
         SharedPreferences sp = context.getSharedPreferences(AUTH_USER_SP, context.MODE_PRIVATE);
         return ModelUtils.convertToObject(sp.getString(ACCESS_USER_SP_KEY, ""), new TypeToken<User>(){});
