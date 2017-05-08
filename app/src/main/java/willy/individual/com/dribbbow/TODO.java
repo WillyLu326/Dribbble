52ae40bbf4d589069024e2b202032d12c7360063
WillyLu326 <luzh0326@gmail.com>
2017-05-07 23:49:04 -0400

    update request

--- app/src/main/java/willy/individual/com/dribbbow/TODO.java
+++ app/src/main/java/willy/individual/com/dribbbow/TODO.java
@@ -9,8 +9,12 @@ public class TODO {
     /**
      *  1. Create Client Model (Optional)
      *  2. Create View Pager in Profile Activity
+     *      a. likes
+     *      b. followers
      *  3. Add Sign Up
-     *  4. Clickable Animation (trigger animation after clicking profile)
+     *  4. Clickable Animation
+     *      a. Follow & Following button clicking
+     *      b. Card view click
      *  5. Check Comment Users' Profile
      *  6. User Self Profile Checking
      *  7. Image zoom in & zoom out
