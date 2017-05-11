79b74f2f955213af31cd2c55bb65fcb89484d091
WillyLu326 <luzh0326@gmail.com>
2017-05-10 22:58:17 -0400

    update

--- app/src/main/java/willy/individual/com/dribbbow/MainActivity.java
+++ app/src/main/java/willy/individual/com/dribbbow/MainActivity.java
@@ -196,7 +196,7 @@ public class MainActivity extends AppCompatActivity {
                 }).show();
 
     }
-//
+
     private class LoadAuthUser extends DribbbleTask<Void, Void, User> {
 
         @Override
